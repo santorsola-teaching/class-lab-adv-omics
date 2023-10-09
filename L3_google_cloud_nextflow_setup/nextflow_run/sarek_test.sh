f0eb3320fbf3ea4407834e5654e3caaa965a5ce1
Mariangela Santorsola <ma.santorsola@gmail.com>
2023-10-09 15:00:14 +0200

    fixed student path

--- L3_google_cloud_nextflow_setup/nextflow_run/sarek_test.sh
+++ L3_google_cloud_nextflow_setup/nextflow_run/sarek_test.sh
@@ -1,6 +1,6 @@
 nextflow run nf-core/sarek -r 3.2.1 \
 -c google.config  \
 -profile test,gls \
---outdir gs://unipv-bioinf-student-msantorsola-data-main/results \
--work-dir gs://unipv-bioinf-student-msantorsola-data-main/results/work
+--outdir resultdir-in-your-bucket \
+-work-dir workdir-in-your-bucket
 
