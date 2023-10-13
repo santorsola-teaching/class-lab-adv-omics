522408d9c34de691eeb2e447d6c449fd45c178f1
Mariangela Santorsola <ma.santorsola@gmail.com>
2023-10-13 12:41:12 +0200

    added gsutil

--- L3_google_cloud_nextflow_setup/nextflow_run/sarek_test.sh
+++ L3_google_cloud_nextflow_setup/nextflow_run/sarek_test.sh
@@ -4,3 +4,10 @@ nextflow run nf-core/sarek -r 3.2.1 \
 --outdir resultdir-in-your-bucket \
 -work-dir workdir-in-your-bucket
 
+
+
+#To copy files from your bucket to you cloud shell
+
+gsutil cp gs://path/to/your/bucket/dir .
+
+ 
