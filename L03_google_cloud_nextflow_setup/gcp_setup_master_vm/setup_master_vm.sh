4a24e9c52b7043c688b8e1ad13c78fe6d36cbbb2
santorsola-teaching <prof.mariangela.santorsola@universitadipavia.it>
2024-09-29 22:03:48 +0200

    added VM setup

---
+++ L03_google_cloud_nextflow_setup/gcp_setup_master_vm/setup_master_vm.sh
@@ -0,0 +1,47 @@
+###############################################
+## system installation steps ##################
+###############################################
+
+# Each time you spin up a new VM, 
+# you'll need to manually install the following tools 
+# to ensure your environment is properly set up:
+
+
+
+sudo yum install -y git
+sudo yum install -y java-11-openjdk-devel
+
+sudo yum install epel-release
+sudo yum install -y screen
+
+
+## only after these tools are installed
+## you can install nextflow as well
+
+export NXF_VER=24.04.4
+export NXF_MODE=google
+
+
+# create the nfbin directory
+mkdir nfbin
+
+# go to the nfbin directory
+cd nfbin
+
+# download nextflow
+curl https://get.nextflow.io | bash
+
+# navigate back to your $HOME directory
+cd ..
+
+# set the `PATH` environment variable
+export PATH=${PATH}:${PWD}
+#e.g. export PATH=${PATH}:/home/<YOUR_HOME_FOLDER>/nfbin/
+
+### the following assumes the credentials have been previously saved
+### in a key with a name of your choice
+
+export GOOGLE_APPLICATION_CREDENTIALS=/home/<YOUR_HOME_FOLDER>/NAME-OF-YOUR-KEY.json
+
+
+
