1f42dc580dde3405a5ed095b09f6a1a87fa5c63f
santorsola-teaching <prof.mariangela.santorsola@universitadipavia.it>
2026-09-15 15:20:41 +0200

    Updating rnaseq run

---
+++ L08_rnaseq_gcp/rnaseq_run/rnaseq_run.cmd
@@ -0,0 +1,20 @@
+## command lines to run rnaseq
+
+screen
+
+nextflow run nf-core/rnaseq -r 3.16.1 \
+--input  PATH/TO/rnaseq/reads/rnaseq_samplesheet.csv \
+--outdir gs://unipv-bioinf-student-YOURNAME-data-main/rnaseq_results_dir_DATASET \
+-work-dir gs://unipv-bioinf-student-YOURNAME-data-main/rnaseq_work_dir_DATASET \
+--genome GRCh38chr21 \
+--pseudo_aligner salmon \
+--skip_alignment \
+--skip_biotype_qc \
+-c rnaseq_nextflow.config \
+-profile gls \
+--skip_stringtie \
+--skip_bigwig \
+--skip_umi_extract \
+--skip_trimming \
+--skip_fastqc
+
